ad4ad9c767aea6a8c351019dd0cb825ac9d2a84c
DanielYapura <danielyapura@gmail.com>
2021-06-14 09:53:13 -0300

    Se agregaron variables de entorno para conectar con la db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require('sequelize');
 
+// extraer valores de variables.env
+require('dotenv').config({path: 'variables.env'});
 
 // Option 2: Passing parameters separately (other dialects)
-const db = new Sequelize('uptasknodedb', 'sammy', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-  define: {
-      timestamps: false
+const db = new Sequelize(
+  process.env.DB_NOMBRE, 
+  process.env.DB_USER,
+  process.env.DB_PASS, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    define: {
+        timestamps: false
+    }
   }
-});
+);
 
 module.exports = db;
