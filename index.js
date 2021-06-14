9f8bbdc13b63f73e4658e36281f50eb723ae30d7
DanielYapura <danielyapura@gmail.com>
2021-06-14 16:05:06 -0300

    correcion del puerto por defecto

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.use('/', routes() );
 
 
 // Servidor y puerto
-const host = process.env.HOST || 'O.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 
